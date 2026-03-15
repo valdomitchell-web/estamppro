ac1c4fbf9e279761eea4f6e22dc11ac01228da04
Valdo <you@example.com>
2026-03-14 20:51:23 -0500

    Replace main.jsx with clean app entry

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,9 @@
-// web/src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-const el = document.getElementById("root");
-createRoot(el).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
